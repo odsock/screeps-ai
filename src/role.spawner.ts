3c84192c3b7de61d5df76d1560019ee9d1de9fd9
Mike Burnham <mburnham@gci.com>
2021-01-25 16:04:37 -0900

    fix indent style

--- src/role.spawner.ts
+++ src/role.spawner.ts
@@ -1,19 +1,19 @@
 export class RoleSpawner {
-    public static breed(creeps: Creep[], role: string, maxCount: number) {
-        if (creeps.length < maxCount) {
-            let newName = 'creep' + Game.time;
-            console.log(role + ': ' + creeps.length + '/' + maxCount + '. Spawning:' + newName);
-            let result = Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, {memory: {role: role}});
-            console.log('Result: ' + result.toString());
-        }
-
-        if (Game.spawns['Spawn1'].spawning) {
-            let spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-            Game.spawns['Spawn1'].room.visual.text(
-                '🛠️' + spawningCreep.memory.role,
-                Game.spawns['Spawn1'].pos.x + 1,
-                Game.spawns['Spawn1'].pos.y,
-                { align: 'left', opacity: 0.8 });
-        }
+  public static breed(creeps: Creep[], role: string, maxCount: number) {
+    if (creeps.length < maxCount) {
+      let newName = 'creep' + Game.time;
+      console.log(role + ': ' + creeps.length + '/' + maxCount + '. Spawning:' + newName);
+      let result = Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newName, { memory: { role: role } });
+      console.log('Result: ' + result.toString());
     }
+
+    if (Game.spawns['Spawn1'].spawning) {
+      let spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+      Game.spawns['Spawn1'].room.visual.text(
+        '🛠️' + spawningCreep.memory.role,
+        Game.spawns['Spawn1'].pos.x + 1,
+        Game.spawns['Spawn1'].pos.y,
+        { align: 'left', opacity: 0.8 });
+    }
+  }
 }
