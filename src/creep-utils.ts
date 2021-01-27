dadb29a627dbb519f30b81d73bfd69f9540b6eb8
Mike Burnham <mburnham@gci.com>
2021-01-27 12:17:57 -0900

    fix harvest bug

--- src/creep-utils.ts
+++ src/creep-utils.ts
@@ -1,7 +1,7 @@
 export class CreepUtils {
   public static harvest(creep: Creep): void {
     let containersWithEnergy = creep.room.find(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store.getUsedCapacity() > 0 });
-    if (containersWithEnergy) {
+    if (containersWithEnergy.length) {
       if (creep.withdraw(containersWithEnergy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(containersWithEnergy[0], { visualizePathStyle: { stroke: '#ffaa00' } });
       }
