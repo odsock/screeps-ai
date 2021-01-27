e757d66e2bc9ddb96809688ae91ee1e88f026642
Mike Burnham <mburnham@gci.com>
2021-01-27 13:42:16 -0900

    more workers, carry more

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,11 @@
 class Constants {
-  static readonly MAX_CREEPS: number = 4;
-  static readonly BODY_WORKER: BodyPartConstant[] = [WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY];
-  static readonly BODY_HARVESTER: BodyPartConstant[] = [MOVE, MOVE, WORK, WORK, WORK, WORK, WORK, WORK, WORK];
+  static readonly MAX_CREEPS: number = 6;
+  static readonly BODY_WORKER: BodyPartConstant[] = [
+    WORK, WORK, WORK, WORK,
+    MOVE, MOVE, MOVE, MOVE,
+    CARRY, CARRY, CARRY, CARRY];
+  static readonly BODY_HARVESTER: BodyPartConstant[] = [
+    MOVE, MOVE,
+    WORK, WORK, WORK, WORK, WORK, WORK, WORK];
 }
 export default Constants;
