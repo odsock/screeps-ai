bd563a4e32c45ccd50408ecfddc05da096fe86cc
Mike Burnham <mburnham@gci.com>
2021-01-27 21:48:16 -0900

    harvest from ruins and tombstones first

--- src/creep-utils.ts
+++ src/creep-utils.ts
@@ -1,16 +1,61 @@
+import { result } from "lodash";
+
 export class CreepUtils {
   public static harvest(creep: Creep): void {
+    if (!CreepUtils.harvestFromTombstones(creep)) {
+      if (!CreepUtils.harvestFromRuins(creep)) {
+        if (!CreepUtils.harvestFromContainer(creep)) {
+          CreepUtils.harvestFromSource(creep);
+        }
+      }
+    }
+  }
+
+  private static harvestFromContainer(creep: Creep): ScreepsReturnCode {
     let containersWithEnergy = creep.room.find(FIND_STRUCTURES, { filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store.getUsedCapacity() > 0 });
     if (containersWithEnergy.length) {
-      if (creep.withdraw(containersWithEnergy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(containersWithEnergy[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+      let result = creep.withdraw(containersWithEnergy[0], RESOURCE_ENERGY);
+      if (result == ERR_NOT_IN_RANGE) {
+        return creep.moveTo(containersWithEnergy[0], { visualizePathStyle: { stroke: '#ffaa00' } });
       }
+      return result;
     }
-    else {
-      let sources = creep.room.find(FIND_SOURCES);
-      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+    return ERR_NOT_FOUND;
+  }
+
+  private static harvestFromRuins(creep: Creep) {
+    let ruinsWithEnergy = creep.room.find(FIND_RUINS, { filter: (r) => r.store.getUsedCapacity() > 0 });
+    if (ruinsWithEnergy.length) {
+      let result = creep.withdraw(ruinsWithEnergy[0], RESOURCE_ENERGY);
+      if (result == ERR_NOT_IN_RANGE) {
+        return creep.moveTo(ruinsWithEnergy[0], { visualizePathStyle: { stroke: '#ffaa00' } });
       }
+      return result;
     }
+    return ERR_NOT_FOUND;
+  }
+
+  private static harvestFromSource(creep: Creep) {
+    let sources = creep.room.find(FIND_SOURCES);
+    if (sources.length) {
+      let result = creep.harvest(sources[0]);
+      if (result == ERR_NOT_IN_RANGE) {
+        return creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+      }
+      return result;
+    }
+    return ERR_NOT_FOUND;
+  }
+
+  private static harvestFromTombstones(creep: Creep) {
+    let tombstonesWithEnergy = creep.room.find(FIND_TOMBSTONES, { filter: (t) => t.store.getUsedCapacity() > 0 });
+    if (tombstonesWithEnergy.length) {
+      let result = creep.withdraw(tombstonesWithEnergy[0], RESOURCE_ENERGY);
+      if (result == ERR_NOT_IN_RANGE) {
+        return creep.moveTo(tombstonesWithEnergy[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+      }
+      return result;
+    }
+    return ERR_NOT_FOUND;
   }
 }
