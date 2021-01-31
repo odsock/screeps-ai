122c959577382371bf67391cd613cb32f3e10df9
Mike Burnham <odsock@gmail.com>
2021-01-31 03:54:50 -0900

    fewer workers to avoid idle time

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 class Constants {
-  static readonly MAX_CREEPS: number = 8;
+  static readonly MAX_CREEPS: number = 4;
   static readonly BODY_PROFILE_WORKER: BodyPartConstant[] = [WORK, MOVE, CARRY];
   static readonly BODY_PROFILE_HARVESTER: BodyPartConstant[] = [WORK];
 }
