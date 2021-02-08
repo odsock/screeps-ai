0c5e86b86b647af2e6d0d046e3ead6c85a222bdf
Mike Burnham <mburnham@gci.com>
2021-02-08 10:19:33 -0900

    fewer wokers

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 class Constants {
-  static readonly MAX_CREEPS: number = 3;
+  static readonly MAX_CREEPS: number = 2;
   static readonly BODY_PROFILE_WORKER: BodyPartConstant[] = [WORK, MOVE, CARRY];
   static readonly BODY_PROFILE_HARVESTER: BodyPartConstant[] = [WORK];
 
