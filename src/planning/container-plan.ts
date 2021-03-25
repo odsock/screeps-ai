0b4a04b72342d2c20021f0e04669af602d0ab9ac
Mike Burnham <mburnham@gci.com>
2021-03-25 13:40:32 -0800

    linting

--- src/planning/container-plan.ts
+++ src/planning/container-plan.ts
@@ -1,7 +1,5 @@
-import { CreepUtils } from "creep-utils";
 import { RoomWrapper } from "structures/room-wrapper";
 import { PlannerUtils } from "./planner-utils";
-import { RoadPlan } from "./road-plan";
 
 export class ContainerPlan {
   private readonly room: RoomWrapper;
@@ -13,7 +11,7 @@ export class ContainerPlan {
   public placeControllerContainer(): ScreepsReturnCode {
     if (this.room.controller && !this.roomHasContainersInConstruction()) {
       const controllerContainer = this.room.controller.pos.findInRange(FIND_STRUCTURES, 1, {
-        filter: (c) => c.structureType === STRUCTURE_CONTAINER
+        filter: c => c.structureType === STRUCTURE_CONTAINER
       });
       if (controllerContainer.length === 0) {
         const id = PlannerUtils.placeStructureAdjacent(this.room.controller.pos, STRUCTURE_CONTAINER);
@@ -46,27 +44,30 @@ export class ContainerPlan {
   private findSourceWithoutContainerCloseToController() {
     if (this.room.controller) {
       return this.room.controller.pos.findClosestByPath(FIND_SOURCES, {
-        filter: (s) => s.pos.findInRange(FIND_STRUCTURES, 1, {
-          filter: (c) => c.structureType === STRUCTURE_CONTAINER
-        }).length === 0
+        filter: s =>
+          s.pos.findInRange(FIND_STRUCTURES, 1, {
+            filter: c => c.structureType === STRUCTURE_CONTAINER
+          }).length === 0
       });
-    }
-    else {
+    } else {
       return null;
     }
   }
 
   private roomHasContainersInConstruction(): boolean {
-    return this.room.find(FIND_MY_CONSTRUCTION_SITES, {
-      filter: (s) => s.structureType === STRUCTURE_CONTAINER
-    }).length > 0;
+    return (
+      this.room.find(FIND_MY_CONSTRUCTION_SITES, {
+        filter: s => s.structureType === STRUCTURE_CONTAINER
+      }).length > 0
+    );
   }
 
   public static findClosestSourceWithoutContainer(pos: RoomPosition): Source | null {
     return pos.findClosestByPath(FIND_SOURCES, {
-      filter: (s) => s.pos.findInRange(FIND_STRUCTURES, 1, {
-        filter: (c) => c.structureType === STRUCTURE_CONTAINER
-      }).length === 0
+      filter: s =>
+        s.pos.findInRange(FIND_STRUCTURES, 1, {
+          filter: c => c.structureType === STRUCTURE_CONTAINER
+        }).length === 0
     });
   }
 }
