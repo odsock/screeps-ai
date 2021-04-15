dee8d48959eaac71266a3eb2555b247a122a10a1
Mike Burnham <mburnham@gci.com>
2021-04-15 11:06:36 -0800

    remove leading space

--- src/structure-patterns.ts
+++ src/structure-patterns.ts
@@ -1,27 +1,27 @@
 /* eslint-disable prettier/prettier */
 export class StructurePatterns {
   public static readonly FULL_COLONY: string[] = [
-    "   r   r   r",
-    "  rer rer rer",
-    " reeereeereeer",
-    "  rererererer",
-    "   reeereeer",
-    "  rerer rerer",
-    " reeer S reeer",
-    "  rerer rerer",
-    "   reeereeer",
-    "  rererererer",
-    " reeereeereeer",
-    "  rer rer rer",
-    "   r   r   r"
+    "  r   r   r",
+    " rer rer rer",
+    "reeereeereeer",
+    " rererererer",
+    "  reeereeer",
+    " rerer rerer",
+    "reeer S reeer",
+    " rerer rerer",
+    "  reeereeer",
+    " rererererer",
+    "reeereeereeer",
+    " rer rer rer",
+    "  r   r   r"
   ];
 
   public static readonly EXTENSION_GROUP: string[] = [
-    "   r",
-    "  rer",
-    " reeer",
-    "  rer",
-    "   r"
+    "  r",
+    " rer",
+    "reeer",
+    " rer",
+    "  r"
   ];
 
   public static readonly CHARACTERS: {[x: string]: StructureConstant | null} = {
@@ -30,4 +30,21 @@ export class StructurePatterns {
     s: STRUCTURE_SPAWN,
     " ": null
   }
+
+public static readonly STRUCTURE_PLAN_EXTENSION_GROUP: StructurePatternPosition[] = [
+  { xOffset: 2, yOffset: 0, structure: STRUCTURE_ROAD },
+  { xOffset: 1, yOffset: 1, structure: STRUCTURE_ROAD },
+  { xOffset: 2, yOffset: 1, structure: STRUCTURE_EXTENSION },
+  { xOffset: 3, yOffset: 1, structure: STRUCTURE_ROAD },
+  { xOffset: 0, yOffset: 2, structure: STRUCTURE_ROAD },
+  { xOffset: 1, yOffset: 2, structure: STRUCTURE_EXTENSION },
+  { xOffset: 2, yOffset: 2, structure: STRUCTURE_EXTENSION },
+  { xOffset: 3, yOffset: 2, structure: STRUCTURE_EXTENSION },
+  { xOffset: 4, yOffset: 2, structure: STRUCTURE_ROAD },
+  { xOffset: 1, yOffset: 3, structure: STRUCTURE_ROAD },
+  { xOffset: 2, yOffset: 3, structure: STRUCTURE_EXTENSION },
+  { xOffset: 3, yOffset: 3, structure: STRUCTURE_ROAD },
+  { xOffset: 2, yOffset: 4, structure: STRUCTURE_ROAD }
+];
+
 }
