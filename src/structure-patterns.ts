fe2de0fa77b1cb666e0b00603e8cc36c73c08e57
Mike Burnham <mburnham@gci.com>
2021-04-15 11:08:13 -0800

    cleanup

--- src/structure-patterns.ts
+++ src/structure-patterns.ts
@@ -30,21 +30,4 @@ export class StructurePatterns {
     s: STRUCTURE_SPAWN,
     " ": null
   }
-
-public static readonly STRUCTURE_PLAN_EXTENSION_GROUP: StructurePatternPosition[] = [
-  { xOffset: 2, yOffset: 0, structure: STRUCTURE_ROAD },
-  { xOffset: 1, yOffset: 1, structure: STRUCTURE_ROAD },
-  { xOffset: 2, yOffset: 1, structure: STRUCTURE_EXTENSION },
-  { xOffset: 3, yOffset: 1, structure: STRUCTURE_ROAD },
-  { xOffset: 0, yOffset: 2, structure: STRUCTURE_ROAD },
-  { xOffset: 1, yOffset: 2, structure: STRUCTURE_EXTENSION },
-  { xOffset: 2, yOffset: 2, structure: STRUCTURE_EXTENSION },
-  { xOffset: 3, yOffset: 2, structure: STRUCTURE_EXTENSION },
-  { xOffset: 4, yOffset: 2, structure: STRUCTURE_ROAD },
-  { xOffset: 1, yOffset: 3, structure: STRUCTURE_ROAD },
-  { xOffset: 2, yOffset: 3, structure: STRUCTURE_EXTENSION },
-  { xOffset: 3, yOffset: 3, structure: STRUCTURE_ROAD },
-  { xOffset: 2, yOffset: 4, structure: STRUCTURE_ROAD }
-];
-
 }
