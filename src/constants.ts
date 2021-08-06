99123cb0f27524ab6a1a63c061b1140a0ff30b52
Mike Burnham <mburnham@gci.com>
2021-08-05 18:09:06 -0800

    add workers

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-  public static readonly MAX_WORKERS: number = 5;
+  public static readonly MAX_WORKERS: number = 8;
   public static readonly WORK_PER_WORKER_PART = 1000;
 
   public static readonly BODY_PROFILE_WORKER: CreepBodyProfile = {
