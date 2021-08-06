5cf9eafcbe720b5e62614cfde54cb8b909aba994
Mike Burnham <mburnham@gci.com>
2021-08-05 22:36:18 -0800

    remove old target info

--- src/target-config.ts
+++ src/target-config.ts
@@ -2,7 +2,8 @@
 export class TargetConfig {
   // TODO: create AI targeting of rooms
 
+  // TODO assume controlled rooms are targets?
   public static readonly TARGETS: {[x: string]: string[]} = {
-    "shard3": ["E12N56", "E11N56"]
+    "shard3": ["E17N55"]
   };
 }
