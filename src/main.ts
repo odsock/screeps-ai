2e610b23ab43dcf54ffb05e2fc83bc10160a2e3b
Mike Burnham <mburnham@gci.com>
2021-08-08 23:29:22 -0800

    import console scripts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Sockpuppet } from "sockpuppet";
 import { ErrorMapper } from "utils/ErrorMapper";
 import { Logger } from "./logger";
 import { Constants } from "./constants";
+import "./utils/console-scripts.js";
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
