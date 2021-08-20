0028bc5fc7da817bc3fd4d75198ddfa26b44a8e0
Mike Burnham <mburnham@gci.com>
2021-08-19 17:48:29 -0800

    reverse test

--- src/creep-utils.ts
+++ src/creep-utils.ts
@@ -7,7 +7,7 @@ export class CreepUtils {
     result: ScreepsReturnCode | undefined = undefined
   ): void {
     if (watchable.memory.watched === true) {
-      if (result === undefined) {
+      if (result !== undefined) {
         const resultString = String(Constants.ERROR_CODE_LOOKUP.get(result));
         console.log(`${watchable.name}: ${message}: ${result} ${resultString}`);
       } else {
