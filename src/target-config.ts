099d2fc9041030d8d01f89e5f3b2ad59861fdf73
Mike Burnham <mburnham@gci.com>
2021-08-30 18:28:07 -0800

    add targets back

--- src/target-config.ts
+++ src/target-config.ts
@@ -4,10 +4,10 @@ export class TargetConfig {
 
   // TODO assume controlled rooms are targets?
   public static readonly TARGETS: {[x: string]: string[]} = {
-    "shard3": []
+    "shard3": ["E17N55", "E18N55"]
   };
 
   public static readonly REMOTE_HARVEST: {[x: string]: string[]} = {
-    "shard3": []
+    "shard3": ["E18N55"]
   };
 }
