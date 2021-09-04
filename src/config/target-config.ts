10418392b5b0a5de6a3e130db2a795de0a7310b1
Mike Burnham <mburnham@gci.com>
2021-09-04 14:40:28 -0800

    update targets

--- src/config/target-config.ts
+++ src/config/target-config.ts
@@ -3,11 +3,11 @@ export class TargetConfig {
   // TODO: create AI targeting of rooms
 
   public static readonly TARGETS: {[x: string]: string[]} = {
-    "shard3": []
+    "shard3": ["E17N55"]
   };
 
   public static readonly REMOTE_HARVEST: {[x: string]: string[]} = {
-    "shard3": ["E18N55", "E17N54"]
+    "shard3": ["E17N55", "E17N54"]
   };
 
   public static readonly IMPORTERS_PER_REMOTE_ROOM = 4;
