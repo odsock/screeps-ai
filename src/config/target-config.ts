88dd6e69542577b837cbbf40f1ad797de52c8c11
Mike Burnham <mburnham@gci.com>
2021-09-05 14:04:48 -0800

    at sim target config

--- src/config/target-config.ts
+++ src/config/target-config.ts
@@ -2,11 +2,13 @@ export class TargetConfig {
   // TODO: create AI targeting of rooms
 
   public static readonly TARGETS: { [x: string]: string[] } = {
-    shard3: ["E17N55"]
+    shard3: ["E17N55"],
+    sim: []
   };
 
   public static readonly REMOTE_HARVEST: { [x: string]: string[] } = {
-    shard3: ["E17N54"]
+    shard3: ["E17N54"],
+    sim: []
   };
 
   // TODO dynamic importer counts
