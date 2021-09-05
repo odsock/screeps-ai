731a92e2118a095dc94f3f9ae9b3e927ed62e939
Mike Burnham <mburnham@gci.com>
2021-09-04 22:55:14 -0800

    update targets

--- src/config/target-config.ts
+++ src/config/target-config.ts
@@ -1,14 +1,14 @@
-/* eslint-disable prettier/prettier */
 export class TargetConfig {
   // TODO: create AI targeting of rooms
 
-  public static readonly TARGETS: {[x: string]: string[]} = {
-    "shard3": ["E17N55"]
+  public static readonly TARGETS: { [x: string]: string[] } = {
+    shard3: ["E17N55"]
   };
 
-  public static readonly REMOTE_HARVEST: {[x: string]: string[]} = {
-    "shard3": ["E17N55", "E17N54"]
+  public static readonly REMOTE_HARVEST: { [x: string]: string[] } = {
+    shard3: ["E17N54"]
   };
 
-  public static readonly IMPORTERS_PER_REMOTE_ROOM = 4;
+  // TODO dynamic importer counts
+  public static readonly IMPORTERS_PER_REMOTE_ROOM = 2;
 }
