f97247d828c32f316933c75cbc2dae3f35be4fa9
Mike Burnham <mburnham@gci.com>
2021-09-24 23:05:31 -0700

    add remote

--- src/config/target-config.ts
+++ src/config/target-config.ts
@@ -8,7 +8,7 @@ export class TargetConfig {
 
   // BUG if you change this config and deploy, queue is not updated
   public static readonly REMOTE_HARVEST: { [x: string]: string[] } = {
-    shard3: ["E17N54"],
+    shard3: ["E17N54", "E18N55"],
     sim: []
   };
 }
