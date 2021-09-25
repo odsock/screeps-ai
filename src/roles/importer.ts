d8b34722b1cb1142990948794b40a614e784075b
Mike Burnham <mburnham@gci.com>
2021-09-25 00:19:03 -0700

    range 1 on move to sign

--- src/roles/importer.ts
+++ src/roles/importer.ts
@@ -39,7 +39,7 @@ export class Importer extends RemoteWorker {
     if (this.room.controller?.sign?.username && this.room.controller.sign.username !== this.owner.username) {
       CreepUtils.consoleLogIfWatched(this, `cpu unsign check ${Game.cpu.getUsed() - cpuBefore}`);
       let cpuDuring = Game.cpu.getUsed();
-      const moveResult = this.moveTo(this.room.controller, { reusePath: 20 });
+      const moveResult = this.moveTo(this.room.controller, { range: 1, reusePath: 20 });
       CreepUtils.consoleLogIfWatched(this, `cpu unsign move ${Game.cpu.getUsed() - cpuDuring}`);
       cpuDuring = Game.cpu.getUsed();
       const inRange = this.pos.isNearTo(this.room.controller);
