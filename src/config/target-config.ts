e93d260557b8c023285ab409280ffabc4aaaab26
Mike Burnham <mburnham@gci.com>
2021-09-26 12:42:31 -0700

    fixed

--- src/config/target-config.ts
+++ src/config/target-config.ts
@@ -6,7 +6,6 @@ export class TargetConfig {
     sim: []
   };
 
-  // BUG if you change this config and deploy, queue is not updated
   public static readonly REMOTE_HARVEST: { [x: string]: string[] } = {
     shard3: ["E17N54", "E18N55"],
     sim: []
