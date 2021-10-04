9094c28e5ab21f61d1f58d4e5cb96ba6d51f599b
Mike Burnham <mburnham@gci.com>
2021-10-03 23:43:09 -0800

    refactor watch script

--- src/utils/console-scripts.ts
+++ src/utils/console-scripts.ts
@@ -1,25 +1,29 @@
 import { Watchable } from "creep-utils";
 
-global.watch = (key: Id<any>) => {
-  let watchable: Watchable | null = Game.getObjectById(key) as Watchable;
-  if (Game.creeps[key]) {
-    watchable = Game.creeps[key] as Watchable;
-  } else if (Game.spawns[key]) {
-    watchable = Game.spawns[key] as Watchable;
-  }
+global.watch = (key: string) => {
+  const watchable: Watchable = getWatchable(key);
   if (watchable) {
     watchable.memory.watched = true;
   }
 };
 
-global.unwatch = (key: Id<any>) => {
-  let watchable: Watchable | null = Game.getObjectById(key) as Watchable;
-  if (Game.creeps[key]) {
-    watchable = Game.creeps[key] as Watchable;
-  } else if (Game.spawns[key]) {
-    watchable = Game.spawns[key] as Watchable;
-  }
+global.unwatch = (key: string) => {
+  const watchable: Watchable = getWatchable(key);
   if (watchable) {
     watchable.memory.watched = false;
   }
 };
+
+function getWatchable(key: string) {
+  let watchable: Watchable;
+  if (Game.rooms[key]) {
+    watchable = Game.rooms[key];
+  } else if (Game.creeps[key]) {
+    watchable = Game.creeps[key] as Watchable;
+  } else if (Game.spawns[key]) {
+    watchable = Game.spawns[key] as Watchable;
+  } else {
+    watchable = Game.getObjectById(key as Id<Creep | Structure>) as Watchable;
+  }
+  return watchable;
+}
