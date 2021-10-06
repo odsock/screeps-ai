8d34abf96af468c4f77ad1d49be314f35422aa99
Mike Burnham <mburnham@gci.com>
2021-10-05 23:16:30 -0800

    don't reduce empty array

--- src/control/spawn-control.ts
+++ src/control/spawn-control.ts
@@ -385,14 +385,18 @@ export class SpawnControl {
     type: typeof Upgrader | typeof Harvester
   ): ScreepsReturnCode {
     const ticksToReplaceHarvester = SpawnUtils.calcReplacementTime(type.BODY_PROFILE, spawnw);
-    const oldestHarvester = creeps
-      .filter(c => !c.memory.retiring && c.ticksToLive && c.ticksToLive <= ticksToReplaceHarvester)
-      .reduce((oldest, c) => {
-        if (c.ticksToLive && oldest.ticksToLive && c.ticksToLive < oldest.ticksToLive) {
+    let oldestHarvester: Creep | undefined;
+    const oldHarvesters = creeps.filter(
+      c => !c.memory.retiring && c.ticksToLive && c.ticksToLive <= ticksToReplaceHarvester
+    );
+    if (oldHarvesters.length > 0) {
+      oldestHarvester = oldHarvesters.reduce((oldest, c) => {
+        if (!oldest || (c.ticksToLive && oldest.ticksToLive && c.ticksToLive < oldest.ticksToLive)) {
           return c;
         }
         return oldest;
       });
+    }
     if (oldestHarvester) {
       const body = SpawnUtils.getMaxBody(type.BODY_PROFILE, spawnw);
       const result = spawnw.spawn({ body, role: type.ROLE, replacing: oldestHarvester.name });
