8b96d382b3ecbb5dad5dc180e09944ad37b3c344
Mike Burnham <mburnham@gci.com>
2021-10-10 00:29:31 -0800

    profile hauler control

--- src/control/hauler-control.ts
+++ src/control/hauler-control.ts
@@ -1,5 +1,6 @@
 import { Hauler } from "roles/hauler";
 import { RoomWrapper } from "structures/room-wrapper";
+import { profile } from "../../screeps-typescript-profiler";
 
 export enum TaskType {
   HAUL = "haul"
@@ -10,28 +11,30 @@ export interface Task {
   target: string;
 }
 
+@profile
 export class HaulerControl {
   public run(): void {
     for (const roomName in Game.rooms) {
       const roomw = RoomWrapper.getInstance(roomName);
       const haulers = roomw.find(FIND_MY_CREEPS, { filter: c => c.memory.role === "hauler" }).map(c => new Hauler(c));
 
-      // create haul tasks
-      const creepsToHaul = this.getHaulRequesters(roomw);
+      // find creeps requesting hauling
+      const creepsToHaul = this.findHaulRequesters(roomw);
       // assign to empty hauler with no other task
       const emptyHaulers = haulers.filter(h => h.store.getUsedCapacity() === 0 && !h.memory.hauleeName);
       creepsToHaul.forEach(c => {
         const closestHauler = c.pos.findClosestByPath(emptyHaulers);
         if (closestHauler) {
+          closestHauler.memory.task = { type: TaskType.HAUL, target: c.name };
           closestHauler.memory.hauleeName = c.name;
-          c.memory.haulerName = closestHauler?.name;
+          c.memory.haulerName = closestHauler.name;
           emptyHaulers.splice(emptyHaulers.findIndex(h => h.id === closestHauler.id));
         }
       });
     }
   }
 
-  private getHaulRequesters(roomw: RoomWrapper): Creep[] {
+  private findHaulRequesters(roomw: RoomWrapper): Creep[] {
     return roomw.find(FIND_MY_CREEPS, {
       filter: creep => creep.memory.haulRequested && !creep.memory.haulerName
     });
