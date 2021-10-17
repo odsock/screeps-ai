230929e9d2bf7823e7f3557ca300bb17226df80b
Mike Burnham <mburnham@gci.com>
2021-10-17 15:56:17 -0800

    create remote control

---
+++ src/control/remote-control.ts
@@ -0,0 +1,163 @@
+import { CreepRole } from "config/creep-types";
+import { TargetConfig } from "config/target-config";
+import { CreepUtils } from "creep-utils";
+import { MemoryUtils } from "planning/memory-utils";
+import { Claimer } from "roles/claimer";
+import { Importer } from "roles/importer";
+import { Worker } from "roles/worker";
+import { RoomWrapper } from "structures/room-wrapper";
+import { SpawnUtils } from "./spawn-utils";
+
+export class RemoteControl {
+  public run(): void {
+    for (const roomName in Game.rooms) {
+      const roomw = RoomWrapper.getInstance(roomName);
+
+      if (roomw.controller?.my && roomw.spawns.length > 0) {
+        this.requestSpawns(roomw);
+      }
+    }
+  }
+
+  private requestSpawns(roomw: RoomWrapper) {
+    const spawnQueue = roomw.memory.spawnQueue ?? [];
+
+    // IMPORTER
+    const remoteHarvestRooms = TargetConfig.REMOTE_HARVEST[Game.shard.name].filter(name => {
+      return !Game.rooms[name]?.controller?.my;
+    });
+    for (const targetRoom of remoteHarvestRooms) {
+      const importersNeeded = this.calcImportersNeededForRoom(roomw, targetRoom);
+      const importersOnRoom = _.filter(
+        Game.creeps,
+        creep => creep.memory.role === Importer.ROLE && creep.memory.targetRoom === targetRoom
+      ).length;
+      CreepUtils.consoleLogIfWatched(roomw, `importers for ${targetRoom}: ${importersOnRoom}/${importersNeeded}`);
+      if (importersNeeded > importersOnRoom) {
+        spawnQueue.push({
+          bodyProfile: Importer.BODY_PROFILE,
+          max: true,
+          role: Importer.ROLE,
+          targetRoom,
+          priority: 50
+        });
+      }
+    }
+
+    // CLAIMER
+    const claimers = _.filter(Game.creeps, c => c.memory.role === CreepRole.CLAIMER).map(c => new Claimer(c));
+    const claimerCount =
+      claimers.length + _.filter(Game.spawns, spawn => spawn.spawning?.name.startsWith(Claimer.ROLE)).length;
+    const maxClaimers = this.getMaxClaimerCount();
+    if (claimerCount < maxClaimers) {
+      const remoteTargetRooms = TargetConfig.TARGETS[Game.shard.name].filter(name => {
+        return !Game.rooms[name]?.controller?.my;
+      });
+      for (const roomName of [...remoteTargetRooms, ...remoteHarvestRooms]) {
+        const claimerOnRoom = !!_.filter(
+          Game.creeps,
+          creep => creep.memory.role === Claimer.ROLE && creep.memory.targetRoom === roomName
+        ).length;
+        CreepUtils.consoleLogIfWatched(roomw, `claimer for ${roomName}: ${String(claimerOnRoom)}`);
+        if (!claimerOnRoom) {
+          spawnQueue.push({
+            bodyProfile: Claimer.BODY_PROFILE,
+            max: true,
+            role: Claimer.ROLE,
+            targetRoom: roomName,
+            priority: 40
+          });
+        }
+      }
+    }
+
+    // REMOTE WORKER
+    // spawn one remote worker for each claimed room with no spawn
+    const noSpawnClaimedRooms = _.filter(
+      Game.rooms,
+      room =>
+        room.controller?.owner &&
+        room.controller?.owner.username === roomw.controller?.owner?.username &&
+        room.find(FIND_MY_SPAWNS).length === 0
+    );
+    const remoteWorkers = _.filter(Game.creeps, creep => creep.memory.role === Worker.ROLE && creep.memory.targetRoom);
+    if (remoteWorkers.length < noSpawnClaimedRooms.length) {
+      const targetRoom = noSpawnClaimedRooms.find(
+        room => !remoteWorkers.find(creep => creep.memory.targetRoom === room.name)
+      );
+      if (targetRoom) {
+        spawnQueue.push({
+          bodyProfile: Worker.BODY_PROFILE,
+          max: true,
+          role: Worker.ROLE,
+          targetRoom: targetRoom.name,
+          priority: 40
+        });
+      }
+    }
+
+    roomw.memory.spawnQueue = spawnQueue;
+  }
+
+  private getMaxClaimerCount(): number {
+    const targetRoomNames = TargetConfig.TARGETS[Game.shard.name].concat(TargetConfig.REMOTE_HARVEST[Game.shard.name]);
+    if (targetRoomNames) {
+      return targetRoomNames.filter(roomName => {
+        // validate room name
+        try {
+          new RoomPosition(0, 0, roomName);
+          // can't do this without a creep in the room
+        } catch (error) {
+          console.log(`ERROR: bad target config: ${roomName}`);
+          return false;
+        }
+        // don't spawn claimers for rooms we own
+        if (Game.rooms[roomName]?.controller?.my) {
+          return false;
+        }
+        return true;
+      }).length;
+    }
+    return 0;
+  }
+
+  private calcImportersNeededForRoom(roomw: RoomWrapper, targetRoom: string): number {
+    // return cached value if room capacity hasn't changed
+    const remoteHarvestRoomMemory = roomw.memory.remoteHarvest?.[targetRoom];
+    if (remoteHarvestRoomMemory?.spawnCapacity === roomw.energyCapacityAvailable) {
+      return remoteHarvestRoomMemory.importersNeeded;
+    }
+
+    // use importer as scout if no recon for room
+    const roomMemory = Memory.rooms[targetRoom];
+    if (!roomMemory) {
+      return 1;
+    }
+    // don't spawn importers for owned rooms, or reserved by other players
+    if (
+      roomMemory.controller?.owner ||
+      roomMemory.controller?.reservation?.username !== roomw.controller?.owner?.username
+    ) {
+      return 0;
+    }
+
+    // spawn enough importers at current max size to maximize harvest
+    const importerBody = SpawnUtils.getMaxBody(Importer.BODY_PROFILE, roomw);
+    const energyCapacity = importerBody.filter(part => part === CARRY).length * CARRY_CAPACITY;
+    const ticksToFill = energyCapacity / (importerBody.filter(part => part === WORK).length * HARVEST_POWER);
+
+    const dropPos = roomw.storage?.pos ?? roomw.spawns[0].pos;
+    const sources = Memory.rooms[targetRoom].sources;
+    let importersNeeded = 0;
+    for (const sourceId in sources) {
+      const sourcePos = MemoryUtils.unpackRoomPosition(sources[sourceId].pos);
+      const path = PathFinder.search(dropPos, sourcePos);
+      importersNeeded += (10 * (path.path.length * 2 + ticksToFill)) / energyCapacity;
+    }
+    // cache result in memory
+    const remoteHarvestRoom = { importersNeeded, spawnCapacity: roomw.energyCapacityAvailable };
+    roomw.memory.remoteHarvest = roomw.memory.remoteHarvest ?? {};
+    roomw.memory.remoteHarvest[targetRoom] = remoteHarvestRoom;
+    return importersNeeded;
+  }
+}
--- src/control/spawn-control.ts
+++ src/control/spawn-control.ts
@@ -1,18 +1,14 @@
-import { Builder } from "roles/builder";
-import { Claimer } from "roles/claimer";
-import { CreepBodyProfile } from "roles/creep-wrapper";
 import { CreepRole } from "config/creep-types";
 import { CreepUtils } from "creep-utils";
+import { Builder } from "roles/builder";
+import { CreepBodyProfile } from "roles/creep-wrapper";
 import { Fixer } from "roles/fixer";
-import { Importer } from "roles/importer";
-import { MemoryUtils } from "planning/memory-utils";
+import { Worker } from "roles/worker";
 import { RoomWrapper } from "structures/room-wrapper";
+import { SpawnWrapper } from "structures/spawn-wrapper";
+import { profile } from "../../screeps-typescript-profiler";
 import { SockPuppetConstants } from "../config/sockpuppet-constants";
 import { SpawnUtils } from "./spawn-utils";
-import { SpawnWrapper } from "structures/spawn-wrapper";
-import { TargetConfig } from "config/target-config";
-import { Worker } from "roles/worker";
-import { profile } from "../../screeps-typescript-profiler";
 
 export interface SpawnRequest {
   priority: number;
@@ -40,16 +36,6 @@ export class SpawnControl {
     for (const role of Object.values(CreepRole)) {
       this.creepCountsByRole[role] = this.getCreepCountForRole(role);
     }
-
-    const importers = _.filter(Game.creeps, c => c.memory.role === CreepRole.IMPORTER).map(c => new Importer(c));
-    const importerCount =
-      importers.length + _.filter(Game.spawns, spawn => spawn.spawning?.name.startsWith(Importer.ROLE)).length;
-    this.creepCountsByRole[CreepRole.IMPORTER] = importerCount;
-
-    const claimers = _.filter(Game.creeps, c => c.memory.role === CreepRole.CLAIMER).map(c => new Claimer(c));
-    const claimerCount =
-      claimers.length + _.filter(Game.spawns, spawn => spawn.spawning?.name.startsWith(Claimer.ROLE)).length;
-    this.creepCountsByRole[CreepRole.CLAIMER] = claimerCount;
   }
 
   public run(): void {
@@ -109,122 +95,6 @@ export class SpawnControl {
     }
   }
 
-  private spawnRemoteEconomy(): void {
-    // IMPORTER
-    const remoteHarvestRooms = TargetConfig.REMOTE_HARVEST[Game.shard.name].filter(name => {
-      return !Game.rooms[name]?.controller?.my;
-    });
-    for (const roomName of remoteHarvestRooms) {
-      const importersNeeded = this.calcImportersNeededForRoom(roomName);
-      const importersOnRoom = _.filter(
-        Game.creeps,
-        creep => creep.memory.role === Importer.ROLE && creep.memory.targetRoom === roomName
-      ).length;
-      CreepUtils.consoleLogIfWatched(this.roomw, `importers for ${roomName}: ${importersOnRoom}/${importersNeeded}`);
-      if (importersNeeded > importersOnRoom) {
-        this.creepCountsByRole[CreepRole.IMPORTER] += 1;
-        this.spawnQueue.push({
-          bodyProfile: Importer.BODY_PROFILE,
-          max: true,
-          role: Importer.ROLE,
-          targetRoom: roomName,
-          priority: 50
-        });
-      }
-    }
-
-    // CLAIMER
-    const maxClaimers = this.getMaxClaimerCount();
-    if (this.creepCountsByRole[CreepRole.CLAIMER] < maxClaimers) {
-      const remoteTargetRooms = TargetConfig.TARGETS[Game.shard.name].filter(name => {
-        return !Game.rooms[name]?.controller?.my;
-      });
-      for (const roomName of [...remoteTargetRooms, ...remoteHarvestRooms]) {
-        const claimerOnRoom = !!_.filter(
-          Game.creeps,
-          creep => creep.memory.role === Claimer.ROLE && creep.memory.targetRoom === roomName
-        ).length;
-        CreepUtils.consoleLogIfWatched(this.roomw, `claimer for ${roomName}: ${String(claimerOnRoom)}`);
-        if (!claimerOnRoom) {
-          this.creepCountsByRole[CreepRole.CLAIMER] += 1;
-          this.spawnQueue.push({
-            bodyProfile: Claimer.BODY_PROFILE,
-            max: true,
-            role: Claimer.ROLE,
-            targetRoom: roomName,
-            priority: 40
-          });
-        }
-      }
-    }
-
-    // REMOTE WORKER
-    // spawn one remote worker for each claimed room with no spawn
-    const noSpawnClaimedRooms = _.filter(
-      Game.rooms,
-      room =>
-        room.controller?.owner &&
-        room.controller?.owner.username === this.roomw.controller?.owner?.username &&
-        room.find(FIND_MY_SPAWNS).length === 0
-    );
-    const remoteWorkers = _.filter(Game.creeps, creep => creep.memory.role === Worker.ROLE && creep.memory.targetRoom);
-    if (remoteWorkers.length < noSpawnClaimedRooms.length) {
-      const targetRoom = noSpawnClaimedRooms.find(
-        room => !remoteWorkers.find(creep => creep.memory.targetRoom === room.name)
-      );
-      if (targetRoom) {
-        this.creepCountsByRole[CreepRole.WORKER] += 1;
-        this.spawnQueue.push({
-          bodyProfile: Worker.BODY_PROFILE,
-          max: true,
-          role: Worker.ROLE,
-          targetRoom: targetRoom.name,
-          priority: 40
-        });
-      }
-    }
-  }
-
-  private calcImportersNeededForRoom(roomName: string): number {
-    // return cached value if room capacity hasn't changed
-    const remoteHarvestRoomMemory = this.roomw.memory.remoteHarvest?.[roomName];
-    if (remoteHarvestRoomMemory?.spawnCapacity === this.roomw.energyCapacityAvailable) {
-      return remoteHarvestRoomMemory.importersNeeded;
-    }
-
-    // use importer as scout if no recon for room
-    const roomMemory = Memory.rooms[roomName];
-    if (!roomMemory) {
-      return 1;
-    }
-    // don't spawn importers for owned rooms, or reserved by other players
-    if (
-      roomMemory.controller?.owner ||
-      roomMemory.controller?.reservation?.username !== this.roomw.controller?.owner?.username
-    ) {
-      return 0;
-    }
-
-    // spawn enough importers at current max size to maximize harvest
-    const importerBody = SpawnUtils.getMaxBody(Importer.BODY_PROFILE, this.roomw);
-    const energyCapacity = importerBody.filter(part => part === CARRY).length * CARRY_CAPACITY;
-    const ticksToFill = energyCapacity / (importerBody.filter(part => part === WORK).length * HARVEST_POWER);
-
-    const dropPos = this.roomw.storage?.pos ?? this.roomw.spawns[0].pos;
-    const sources = Memory.rooms[roomName].sources;
-    let importersNeeded = 0;
-    for (const sourceId in sources) {
-      const sourcePos = MemoryUtils.unpackRoomPosition(sources[sourceId].pos);
-      const path = PathFinder.search(dropPos, sourcePos);
-      importersNeeded += (10 * (path.path.length * 2 + ticksToFill)) / energyCapacity;
-    }
-    // cache result in memory
-    const remoteHarvestRoom = { importersNeeded, spawnCapacity: this.roomw.energyCapacityAvailable };
-    this.roomw.memory.remoteHarvest = this.roomw.memory.remoteHarvest ?? {};
-    this.roomw.memory.remoteHarvest[roomName] = remoteHarvestRoom;
-    return importersNeeded;
-  }
-
   /**
    * Spawn strategy for later RCL
    * spawn same as RC1, with guards, builders, importers, claimers, and fixer
@@ -246,8 +116,6 @@ export class SpawnControl {
         priority: 30
       });
     }
-
-    this.spawnRemoteEconomy();
 
     // BUILDER
     // make builders if there's something to build
@@ -287,30 +155,6 @@ export class SpawnControl {
         });
       }
     });
-  }
-
-  /** plan creep count functions */
-
-  private getMaxClaimerCount(): number {
-    const targetRoomNames = TargetConfig.TARGETS[Game.shard.name].concat(TargetConfig.REMOTE_HARVEST[Game.shard.name]);
-    if (targetRoomNames) {
-      return targetRoomNames.filter(roomName => {
-        // validate room name
-        try {
-          new RoomPosition(0, 0, roomName);
-          // can't do this without a creep in the room
-        } catch (error) {
-          console.log(`ERROR: bad target config: ${roomName}`);
-          return false;
-        }
-        // don't spawn claimers for rooms we own
-        if (Game.rooms[roomName]?.controller?.my) {
-          return false;
-        }
-        return true;
-      }).length;
-    }
-    return 0;
   }
 
   /** calculate creep bodies */
