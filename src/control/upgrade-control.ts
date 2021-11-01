7fa4cf469369a9e8627133f776dae3add48a17c9
Mike Burnham <mburnham@gci.com>
2021-11-01 01:23:01 -0800

    simplify upgrader spawning, cap parts at rcl 8

--- src/control/upgrade-control.ts
+++ src/control/upgrade-control.ts
@@ -19,50 +19,43 @@ export class UpgradeControl {
     }
   }
 
-  private requestSpawns(roomw: RoomWrapper) {
+  private requestSpawns(roomw: RoomWrapper): void {
+    if (roomw.find(FIND_MY_CONSTRUCTION_SITES).length > 0) {
+      CreepUtils.consoleLogIfWatched(roomw, `skipping upgraders during construction`);
+      return;
+    }
     const spawnQueue = SpawnQueue.getInstance(roomw);
 
     const upgraderCount = SpawnUtils.getCreepCountForRole(roomw, CreepRole.UPGRADER);
-    const bodyProfile = Upgrader.BODY_PROFILE;
-    if (roomw.controller?.level === 8) {
-      // can only upgrade 15 points per tick at rcl 8
-      bodyProfile.maxBodyParts = 16;
-    }
+    let upgradePositionCount = 1;
+    let upgraderWorkPartsActive = 0;
+    const upgraderWorkPartsNeeded =
+      roomw.controller?.level === 8
+        ? CONTROLLER_MAX_UPGRADE_PER_TICK / UPGRADE_CONTROLLER_POWER
+        : roomw.sourcesEnergyCapacity / ENERGY_REGEN_TIME / UPGRADE_CONTROLLER_POWER;
 
-    // FIRST UPGRADER
-    // start upgrading once harvesting efficiently
-    if (upgraderCount === 0) {
-      spawnQueue.push({
-        bodyProfile: Upgrader.BODY_PROFILE,
-        max: true,
-        memory: { role: Upgrader.ROLE },
-        priority: 80
-      });
-    }
-
-    // UPGRADER
-    // spawn enough upgraders to match source capacity
-    // don't spawn upgraders during construction
-    if (roomw.find(FIND_MY_CONSTRUCTION_SITES).length > 0 && upgraderCount > 0) {
-      CreepUtils.consoleLogIfWatched(roomw, `skipping upgraders during construction`);
-    } else {
-      const upgradePositionCount = Math.min(SockPuppetConstants.MAX_UPGRADERS, roomw.getUpgradePositions().length);
-      const upgraderWorkPartsNeeded = roomw.sourcesEnergyCapacity / ENERGY_REGEN_TIME / UPGRADE_CONTROLLER_POWER;
-      const upgraderWorkPartsActive = roomw.getActiveParts(Upgrader.ROLE, WORK);
+    if (upgraderCount > 0) {
+      upgradePositionCount = Math.min(SockPuppetConstants.MAX_UPGRADERS, roomw.getUpgradePositions().length);
+      upgraderWorkPartsActive = roomw.getActiveParts(Upgrader.ROLE, WORK);
       CreepUtils.consoleLogIfWatched(
         roomw,
         `upgraders: ${upgraderCount}/${upgradePositionCount} positions, ${upgraderWorkPartsActive}/${upgraderWorkPartsNeeded} parts`
       );
-      if (upgraderWorkPartsActive < upgraderWorkPartsNeeded && upgraderCount < upgradePositionCount) {
-        const bodyProfile = SpawnUtils.buildBodyProfile(Upgrader.BODY_PROFILE, upgraderWorkPartsNeeded);
-        spawnQueue.push({
-          bodyProfile,
-          memory: { role: Upgrader.ROLE },
-          priority: 80
-        });
-      } else if (upgraderCount <= upgradePositionCount) {
-        SpawnUtils.requestReplacementCreep(roomw, Upgrader);
-      }
+    }
+
+    if (
+      upgraderCount === 0 ||
+      (upgraderWorkPartsActive < upgraderWorkPartsNeeded && upgraderCount < upgradePositionCount)
+    ) {
+      spawnQueue.push({
+        bodyProfile: SpawnUtils.buildBodyProfile(
+          Upgrader.BODY_PROFILE,
+          upgraderWorkPartsNeeded - upgraderWorkPartsActive
+        ),
+        max: true,
+        memory: { role: Upgrader.ROLE },
+        priority: 80
+      });
     }
   }
 }
