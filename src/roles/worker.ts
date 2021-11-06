7df63445e8aa1642c88628bf07b4474a99b6192e
Mike Burnham <mburnham@gci.com>
2021-11-06 13:08:40 -0800

    back out worker role swap

--- src/roles/worker.ts
+++ src/roles/worker.ts
@@ -22,13 +22,6 @@ export class Worker extends CreepWrapper {
       return;
     }
 
-    // become a harvester if have haulers now
-    const haulers = this.roomw.creeps.filter(c => c.memory.role === CreepRole.HAULER);
-    if (this.roomw.controller?.level === 1 && haulers.length > 0) {
-      this.memory.role = CreepRole.HARVESTER;
-      this.memory.source = this.pos.findClosestByPath(this.roomw.sources)?.id;
-    }
-
     // harvest if any capacity in room
     if (this.room.energyAvailable < this.room.energyCapacityAvailable) {
       CreepUtils.consoleLogIfWatched(this, "harvesting job");
