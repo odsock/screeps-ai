6a41d71058561006c45300da164c698826887499
Mike Burnham <mburnham@gci.com>
2021-12-06 10:17:28 -0900

    debug

--- src/control/task-management.ts
+++ src/control/task-management.ts
@@ -76,7 +76,10 @@ export class TaskManagement {
       }
     });
     const newTasks = tasks.filter(t => !busyHaulers.some(h => this.isDuplicateTask(h.getTask(), t)));
-    CreepUtils.consoleLogIfWatched(haulers[0].room, `found ${tasks.length} tasks, ${tasks.length} new tasks`);
+    CreepUtils.consoleLogIfWatched(
+      haulers[0].room,
+      `${busyHaulers.length}/${freeHaulers.length} busy/free, found ${tasks.length} tasks, ${tasks.length} new tasks`
+    );
     const tasksByPriority = newTasks.sort((a, b) => b.priority - a.priority);
 
     const unassignedTasks: Task[] = [];
