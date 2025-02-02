ffc05d6c730ead0cb28c1c1633352fc18d924cff
Mike Burnham <mburnham@gci.com>
2025-02-02 01:40:54 -0900

    linting config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,4 +4,13 @@ import eslint from "@eslint/js";
 import prettierConfig from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, prettierConfig);
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  prettierConfig,
+  {
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error"
+    }
+  }
+);
